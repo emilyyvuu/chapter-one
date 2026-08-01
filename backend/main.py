cbd20cc8da6ad26e38d5f9d645fc55baa36fd8fe
Emily Vu <emilyvu@vt.edu>
2026-08-01 12:58:39 -0400

    generate embedding when a book is added

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,15 @@ from supabase import Client
 from supabase_client import get_supabase
 from dotenv import load_dotenv
 from schemas.books import Books
+from openai import OpenAI
 
 load_dotenv()
 
 GOOGLE_BOOKS_API_KEY = os.environ["GOOGLE_BOOKS_API_KEY"]
-
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+ 
 app = FastAPI()
+open_ai_client = OpenAI()
 
 @app.get("/")
 def read_root():
@@ -66,13 +69,24 @@ def add_book(book: Books, rating: int):
     if book_check.data:
         book_check_id = book_check.data[0]["id"]
     else:
-        response = supabase.table("books").insert(book.model_dump()).execute()
+        embedding_input = book.description
+
+        if embedding_input is None:
+            embedding_input =  " ".join(book.author + [book.title])
+
+        generate_embedding = open_ai_client.embeddings.create(
+                                input=embedding_input, 
+                                model="text-embedding-3-small"
+                            )
+        book_model = book.model_dump()
+        book_model["embedding"] = generate_embedding.data[0].embedding
+        response = supabase.table("books").insert(book_model).execute()
         book_check_id = response.data[0]["id"]
 
     rating_response = supabase.table("ratings").insert({
         "book_id": book_check_id,
         "rating": rating,
-        "user_id": "1"
+        "user_id": 1
     }).execute()
 
     return rating_response.data
